e1636b2ff9899d0fa5036a91e6402e31e893b546
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-10-17 21:07:24 -0700

    fix link

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -10,13 +10,13 @@ export default function ProjectPage() {
 		<div className="lg:p-8 p-4 shadow-xl rounded-xl bg-lochmara-200 m-2 mt-8 lg:m-8">
 			<div className="text-4xl font-bold mb-4">Web Toys</div>
 			<div className="flex-1 grid md:grid-cols-2 xl:grid-cols-2 gap-4 md:gap-8 ">
-				<a href="/statistics/">
+				<Link href="/statistics/">
 					<div className="bg-lochmara-100 p-8 shadow-xl rounded-xl mt-4 border-lochmara-300 border-4 inline-block min-w-full">
 						<h3 className="text-2xl font-bold">Hypixel Level Statistics</h3>
 						<h4>This web app calculates statistics for Hypixel levels and plots the change over time. </h4>
 						<Image src={statisticsImage} alt="Hypixel Level Statistics Image" width={600} />
 					</div>
-				</a>
+				</Link>
 
 				<Link href="/recentgames/">
 					<div className="bg-lochmara-100 p-8 shadow-xl rounded-xl mt-4 border-lochmara-300 border-4 inline-block min-w-full">
