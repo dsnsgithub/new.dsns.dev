0906de616ae3353ce957cbb7f4dba93e7354f88d
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-12-16 22:46:23 -0800

    allow downloading restricted youtube videos

--- src/pages/api/youtubeVideo/[id].ts
+++ src/pages/api/youtubeVideo/[id].ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import ytdl from "ytdl-core";
+import ytdl from "@distube/ytdl-core";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
 	const { id } = req.query;
