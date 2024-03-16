251ccc6bf83e92b8de7ebf4f6c290740761f9979
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-03-16 16:51:27 -0700

    fix

--- src/pages/components/CustomTags.tsx
+++ src/pages/components/CustomTags.tsx
@@ -8,7 +8,7 @@ export default function CustomTags(props: {
 		<Head>
 			<title>dsns.dev - {props.title}</title>
 			<meta property="og:type" content="website" />
-			<meta property="og:title" content={`dsns.dev - {props.title}`} />
+			<meta property="og:title" content={`dsns.dev - ${props.title}`} />
 			<meta property="og:description" content={props.description} />
 			<meta name="description" content={props.description} />
 			<meta property="og:url" content="https://www.dsns.dev" />
