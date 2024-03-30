dfb34b36634c73a06fa7f0016f45c52831fa31ce
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-03-29 20:09:37 -0700

    add even more animations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import Navbar from "./components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 
 import { Poppins } from "next/font/google";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [elem] = useAutoAnimate({duration: 100});
 	return (
 		<>
-			<main className={"min-h-screen container mx-auto mt-10 " + poppins.className}>
+			<main className={"min-h-screen container mx-auto mt-10 " + poppins.className} ref={elem}>
 				<Navbar />
 				<Component {...pageProps} />
 				<Analytics />
