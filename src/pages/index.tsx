732a2f54838939037f95b14128c982735fa40e74
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-03-29 20:07:17 -0700

    add some sweet animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import { FaCloudflare, FaGithub, FaJsSquare, FaPython, FaYoutube, FaLinux, FaGitAlt, FaReact, FaEnvelope } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import { useLanyard } from "react-use-lanyard";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+import rawGames from "./db/gameList.json";
+const gameList = rawGames as { [key: string]: string };
 
 import Image, { StaticImageData } from "next/image";
 import scheduliImage from "../../public/images/scheduli.png";
 import hackathonImage from "../../public/images/hackathon.png";
-
 import chromocraftImage from "../../public/images/chromocraft.png";
 import truthBeToldImage from "../../public/images/TruthBeTold.jpg";
 import onlyEggrollsImage from "../../public/images/onlyeggrolls.png";
 import CustomTags from "./components/CustomTags";
-
-import { useLanyard } from "react-use-lanyard";
-import rawGames from "./db/gameList.json";
-const gameList = rawGames as { [key: string]: string };
-import { useState } from "react";
 
 function FeatureCard(props: { link: string; title: string; description: string; image: StaticImageData; imageWidth: number; flipped: boolean }) {
 	return (
@@ -67,21 +67,29 @@ function cutStrings(string: string, maxLength: number) {
 }
 
 export default function Home() {
+	const [outerStatusDiv] = useAutoAnimate({ duration: 500 });
+	const [outerStatus2Div] = useAutoAnimate({ duration: 500 });
+	const [innerStatusDiv] = useAutoAnimate({ duration: 500 });
+	const [statusVisible, setStatusVisible] = useState(false);
+
 	const { loading, status } = useLanyard({
 		userId: "342874998375186432",
 		socket: true
 	});
 
 	const [currentTime, setCurrentTime] = useState(new Date().getTime());
-
 	setInterval(() => setCurrentTime(new Date().getTime()), 1000);
+
+	useEffect(() => {
+		setTimeout(() => setStatusVisible(true), 500);
+	}, []);
 
 	return (
 		<div>
 			<CustomTags title="Home" description="Check out what I do, and explore some of my projects."></CustomTags>
 
 			<div className="lg:flex lg:flex-row justify-evenly items-center">
-				<div className="flex flex-col justify-center lg:p-8 shadow-xl rounded-xl mt-4 bg-lochmara-200 lg:m-8 m-2">
+				<div className="flex flex-col justify-center lg:p-8 shadow-xl rounded-xl mt-4 bg-lochmara-200 lg:m-8 m-2" ref={outerStatus2Div}>
 					<div className="bg-lochmara-100 shadow-xl rounded-xl mb-4 p-6 border-lochmara-300 border-4">
 						<div className="text-4xl font-bold mb-4">Who am I?</div>
 						<div className="text-xl">A full-stack developer with an interest in Minecraft.</div>
@@ -118,76 +126,81 @@ export default function Home() {
 						</div>
 					</div>
 
-					{loading || !status?.activities || status.activities.length == 0 ? (
-						<></>
-					) : (
-						<div className="bg-lochmara-100 shadow-xl rounded-xl mt-4 p-6 border-lochmara-300 border-4">
-							<div className="text-4xl font-bold mb-4">Status</div>
-							<div>
-								{status.activities[0].name === "Spotify" && status.spotify ? (
-									<>
-										<h3 className="font-bold mb-2">Listening to Spotify</h3>
+					<div ref={outerStatusDiv}>
+						{loading || !status?.activities || status.activities.length == 0 || !statusVisible ? (
+							<></>
+						) : (
+							<div className="bg-lochmara-100 shadow-xl rounded-xl mt-4 p-6 border-lochmara-300 border-4">
+								<div className="text-4xl font-bold mb-4">Status</div>
+								<div ref={innerStatusDiv}>
+									{status.activities[0].name === "Spotify" && status.spotify ? (
+										<>
+											<h3 className="font-bold mb-2">Listening to Spotify</h3>
 
+											<div className="flex items-center space-x-4">
+												<img src={status.spotify?.album_art_url} alt="Album Art" className="w-16 h-16 rounded" />
+												<div>
+													<h4>{cutStrings(status.spotify?.song, 16)}</h4>
+													<p className="text-sm">by {cutStrings(status.spotify?.artist, 16)}</p>
+													<p className="text-sm">on {cutStrings(status.spotify?.album, 16)}</p>
+												</div>
+											</div>
+											<div className="flex items-center space-x-2 mt-2">
+												<span>
+													{formatTime(
+														Math.min(
+															currentTime - (status.spotify?.timestamps?.start || 0),
+															(status.spotify?.timestamps?.end || 0) - (status.spotify?.timestamps?.start || 0)
+														)
+													)}
+												</span>
+												<progress
+													value={currentTime - (status.spotify?.timestamps?.start || 0)}
+													max={(status.spotify?.timestamps?.end || 0) - (status.spotify?.timestamps?.start || 0)}
+													className="w-3/4 rounded-xl [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-value]:rounded-lg   [&::-webkit-progress-bar]:bg-slate-300 [&::-webkit-progress-value]:bg-lochmara-400 [&::-moz-progress-bar]:bg-lochmara-400"
+												></progress>
+												<span>{formatTime((status.spotify?.timestamps?.end || 0) - (status.spotify?.timestamps?.start || 0))}</span>
+											</div>
+										</>
+									) : (
 										<div className="flex items-center space-x-4">
-											<img src={status.spotify?.album_art_url} alt="Album Art" className="w-16 h-16 rounded" />
-											<div>
-												<h4>{cutStrings(status.spotify?.song, 16)}</h4>
-												<p className="text-sm">by {cutStrings(status.spotify?.artist, 16)}</p>
-												<p className="text-sm">on {cutStrings(status.spotify?.album, 16)}</p>
-											</div>
-										</div>
-										<div className="flex items-center space-x-2 mt-2">
-											<span>
-												{formatTime(
-													Math.min(currentTime - (status.spotify?.timestamps?.start || 0), (status.spotify?.timestamps?.end || 0) - (status.spotify?.timestamps?.start || 0))
-												)}
-											</span>
-											<progress
-												value={currentTime - (status.spotify?.timestamps?.start || 0)}
-												max={(status.spotify?.timestamps?.end || 0) - (status.spotify?.timestamps?.start || 0)}
-												className="w-3/4 rounded-xl [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-value]:rounded-lg   [&::-webkit-progress-bar]:bg-slate-300 [&::-webkit-progress-value]:bg-lochmara-400 [&::-moz-progress-bar]:bg-lochmara-400"
-											></progress>
-											<span>{formatTime((status.spotify?.timestamps?.end || 0) - (status.spotify?.timestamps?.start || 0))}</span>
-										</div>
-									</>
-								) : (
-									<div className="flex items-center space-x-4">
-										{status.activities[0].assets ? (
-											<div className="flex-shrink-0 relative">
-												<img
-													src={`https://cdn.discordapp.com/app-assets/${status.activities[0]?.application_id}/${status.activities[0].assets?.large_image}.png`}
-													alt="Activity Image"
-													className="w-16 h-16 rounded"
-												/>
-												<img
-													src={`https://cdn.discordapp.com/app-assets/${status.activities[0]?.application_id}/${status.activities[0].assets?.small_image}.png`}
-													alt="Activity Image"
-													className="w-6 h-6 rounded right-0 bottom-0 absolute ring-3"
-												/>
-											</div>
-										) : gameList[status.activities[0].application_id || ""] ? (
-											<>
-												<img
-													src={`https://cdn.discordapp.com/app-icons/${status.activities[0]?.application_id}/${gameList[status.activities[0].application_id || ""]}.png`}
-													alt="Activity Image"
-													className="w-16 h-16 rounded"
-												/>
-											</>
-										) : (
-											<></>
-										)}
+											{status.activities[0].assets ? (
+												<div className="flex-shrink-0 relative">
+													<img
+														src={`https://cdn.discordapp.com/app-assets/${status.activities[0]?.application_id}/${status.activities[0].assets?.large_image}.png`}
+														alt="Activity Image"
+														className="w-16 h-16 rounded"
+													/>
+													<img
+														src={`https://cdn.discordapp.com/app-assets/${status.activities[0]?.application_id}/${status.activities[0].assets?.small_image}.png`}
+														alt="Activity Image"
+														className="w-6 h-6 rounded right-0 bottom-0 absolute ring-3"
+													/>
+												</div>
+											) : gameList[status.activities[0].application_id || ""] ? (
+												<>
+													<img
+														src={`https://cdn.discordapp.com/app-icons/${status.activities[0]?.application_id}/${gameList[status.activities[0].application_id || ""]}.png`}
+														alt="Activity Image"
+														className="w-16 h-16 rounded"
+													/>
+												</>
+											) : (
+												<></>
+											)}
 
-										<div>
-											<h4 className="font-bold">{status.activities[0].name}</h4>
-											<p className="text-sm">{status.activities[0].state}</p>
-											<p className="text-sm">{status.activities[0].details}</p>
-											<p className="text-sm">{formatTime(currentTime - status.activities[0].created_at)} elapsed</p>
+											<div>
+												<h4 className="font-bold">{status.activities[0].name}</h4>
+												<p className="text-sm">{status.activities[0].state}</p>
+												<p className="text-sm">{status.activities[0].details}</p>
+												<p className="text-sm">{formatTime(currentTime - status.activities[0].created_at)} elapsed</p>
+											</div>
 										</div>
-									</div>
-								)}
+									)}
+								</div>
 							</div>
-						</div>
-					)}
+						)}
+					</div>
 				</div>
 
 				<div className="flex flex-col justify-center p-6 lg:p-8 shadow-xl rounded-xl bg-lochmara-200 m-2 mt-8 lg:m-8">
