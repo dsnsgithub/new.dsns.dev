8e61a7b4253657cd3c624cb392b2e364ed3963a6
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-05-01 22:32:44 -0700

    fix confetti canvas

--- src/pages/birthdays.tsx
+++ src/pages/birthdays.tsx
@@ -67,39 +67,40 @@ export default function Birthdays() {
 	}
 
 	return (
-		<div className="container mx-auto mt-8">
-			{birthdayToday && width && height ? <Confetti width={width} height={height}></Confetti> : <></>}
+		<>
+			{birthdayToday && width && height ? <Confetti width={width} height={height} className="w-full h-full"></Confetti> : <></>}
+			<div className="container mx-auto mt-8">
+				<CustomTags title="Birthdays" description="List of upcoming birthdays"></CustomTags>
 
-			<CustomTags title="Birthdays" description="List of upcoming birthdays"></CustomTags>
+				<div className="lg:p-8 p-4 shadow-xl rounded-xl bg-lochmara-200 m-2 mt-8 lg:m-8">
+					<h1 className="text-3xl font-semibold mb-4">Birthdays</h1>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+						{birthdays.map((birthday) => (
+							<div key={birthday.name} className="bg-lochmara-100 rounded-xl shadow-md p-6">
+								<div className="flex">
+									<Tooltip id={birthday.date.toLocaleDateString()}></Tooltip>
+									<h2 className="text-xl font-semibold mb-2" data-tooltip-id={birthday.date.toLocaleDateString()} data-tooltip-content={birthday.date.toLocaleDateString()}>
+										{birthday.name}
+									</h2>
+								</div>
 
-			<div className="lg:p-8 p-4 shadow-xl rounded-xl bg-lochmara-200 m-2 mt-8 lg:m-8">
-				<h1 className="text-3xl font-semibold mb-4">Birthdays</h1>
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-					{birthdays.map((birthday) => (
-						<div key={birthday.name} className="bg-lochmara-100 rounded-xl shadow-md p-6">
-							<div className="flex">
-								<Tooltip id={birthday.date.toLocaleDateString()}></Tooltip>
-								<h2 className="text-xl font-semibold mb-2" data-tooltip-id={birthday.date.toLocaleDateString()} data-tooltip-content={birthday.date.toLocaleDateString()}>
-									{birthday.name}
-								</h2>
+								<div className="mb-4">
+									{birthday.date.getTime() + 86400000 > new Date().getTime() && birthday.date.getTime() < new Date().getTime() ? (
+										<p className="font-bold">Happy Birthday {birthday.name}!</p>
+									) : (
+										<p>
+											{calculateDaysUntilDate(birthday.date)} day{calculateDaysUntilDate(birthday.date) === 1 ? "" : "s"} ({calculatePercent(birthday.date).toFixed(2)}%)
+										</p>
+									)}
+								</div>
+								<div className="h-4 bg-lochmara-200 border border-lochmara-300 rounded-full overflow-hidden">
+									<div className="bg-lochmara-500 h-full" style={{ width: `${calculatePercent(birthday.date)}%` }}></div>
+								</div>
 							</div>
-
-							<div className="mb-4">
-								{birthday.date.getTime() + 86400000 > new Date().getTime() && birthday.date.getTime() < new Date().getTime() ? (
-									<p className="font-bold">Happy Birthday {birthday.name}!</p>
-								) : (
-									<p>
-										{calculateDaysUntilDate(birthday.date)} day{calculateDaysUntilDate(birthday.date) === 1 ? "" : "s"} ({calculatePercent(birthday.date).toFixed(2)}%)
-									</p>
-								)}
-							</div>
-							<div className="h-4 bg-lochmara-200 border border-lochmara-300 rounded-full overflow-hidden">
-								<div className="bg-lochmara-500 h-full" style={{ width: `${calculatePercent(birthday.date)}%` }}></div>
-							</div>
-						</div>
-					))}
+						))}
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
